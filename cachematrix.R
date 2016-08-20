ae5f46d900613598badee4507650dbd6b8674f90
princebhalla <princebhallatauras20@gmail.com>
2016-08-20 16:46:00 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -23,7 +23,7 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   
-    m <- x$getmean()
+    m <- x$getinverse()
     if(!is.null(m)) {
       message("getting cached data")
       return(m)
